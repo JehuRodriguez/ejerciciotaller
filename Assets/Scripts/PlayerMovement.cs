5c252dd121efe95cdb0d58510f7ae673fca87543
Jesus Rodriguez <j.eh.u14@hotmail.com>
2024-05-02 16:58:18 -0500

    Movimiento del jugador

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    public void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        rb2d.velocity = new Vector2(horizontal, vertical).normalized * speed;
+    }
+}
